755f5cf469a1f587e18a6c05cbbcf75443081326
Mikhail Snytkov <coolfilinz@gmail.com>
2022-10-14 21:36:06 +0300

    added ants

---
+++ ants/src/Main.hx
@@ -0,0 +1,23 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author filinz
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		trace("ANTS!");
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
